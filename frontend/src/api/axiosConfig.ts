5c474c553394d60ae02fbf915368d3d1c7de3866
Tilak Vasu <tilak.vasu@gatewaygroup.com>
2025-08-05 13:52:56 +0530

    Configure for initial Vercel deployment

--- frontend/src/api/axiosConfig.ts
+++ frontend/src/api/axiosConfig.ts
@@ -1,14 +1,7 @@
-// src/api/axiosConfig.ts
-
 import axios from 'axios';
 
-/**
- * Creates a pre-configured instance of Axios.
- * The baseURL is set to point to your Django backend's API endpoint.
- */
 const api = axios.create({
   baseURL: import.meta.env.PROD ? '/api' : 'http://127.0.0.1:8000/api',
- // Make sure this matches your backend's address
 });
 
 export default api;
