c32e3ca2da99674320a7c7a35de6fb14569bae0a
Tilak Vasu <tilak.vasu@gatewaygroup.com>
2025-08-05 14:06:12 +0530

    Configure for initial Vercel deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,14 +78,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
+if 'POSTGRES_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.config(
+            default=os.environ.get('POSTGRES_URL'),
             conn_max_age=600,
             ssl_require=True
         )
     }
 else:
+    # This is for your local development
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
