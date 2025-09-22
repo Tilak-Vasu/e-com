dcc3d0700154ab39103c1ca647933b3f49e2270d
Tilak Vasu <tilak.vasu@thegatewaycorp.co.in>
2025-09-22 13:53:05 +0530

    new134

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,14 +1,18 @@
 // frontend/jest.config.js
 
 module.exports = {
-  // Points Jest to our setup file. It will run this before every test.
-  setupFilesAfterEnv: ['<rootDir>/src/jest.setup.ts'],
-  
   // Specifies the test environment. 'jsdom' simulates a browser environment.
   testEnvironment: 'jest-environment-jsdom',
 
-  // Other Jest configurations can go here...
-  // For example, mapping for CSS files:
+  // Points Jest to our setup file. It will run this before every test.
+  setupFilesAfterEnv: ['<rootDir>/src/jest.setup.ts'],
+
+  // Tells Jest how to handle TypeScript files (.ts, .tsx)
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+
+  // Mocks CSS imports so they don't crash the tests
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
