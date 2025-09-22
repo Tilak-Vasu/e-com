3c5ffda782ae0fdf6b1e34e775e226cef4e53c22
Tilak Vasu <tilak.vasu@thegatewaycorp.co.in>
2025-09-22 14:09:12 +0530

    new1349

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,19 +1,18 @@
-// frontend/jest.config.js
-
+// frontend/jest.config.cjs
 module.exports = {
-  // Specifies the test environment. 'jsdom' simulates a browser environment.
+  preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
-
-  // Points Jest to our setup file. It will run this before every test.
   setupFilesAfterEnv: ['<rootDir>/src/jest.setup.ts'],
-
-  // Tells Jest how to handle TypeScript files (.ts, .tsx)
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-
-  // Mocks CSS imports so they don't crash the tests
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  }
 };
